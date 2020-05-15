0252bddba7728a2f8b322599582a4431649197d3
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-15 18:58:06 +0300

    init

---
+++ config/web.php
@@ -0,0 +1,64 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Vi6PHhj_KvqU7FncAxFQvHIebCwm8zhq',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
---
+++ views/site/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Отклонение';
+?>
+<div class="row no-gutters">
+    <div class="col-2 offset-5">
+
+    </div>
+</div>
